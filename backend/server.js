1b3f0a4395630ad5571483c4127cc36689a750ea
Pritchad25 <pricts29@gmail.com>
2025-05-05 10:11:15 +0000

    Basic PharmaNex backend server

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express'); 
+const app = express(); 
+
+// Definition of the GET route for the URL
+app.get('/', (req, res) => {
+	res.send('PharmaNex Server is running');
+});
+
+app.listen(3000, () => {
+	console.log('PharmaNex Server started on port 3000');
+});
