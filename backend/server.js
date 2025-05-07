e928914241d692a7deeaeacd267bf46cf67e5441
Pritchad25 <pricts29@gmail.com>
2025-05-07 12:04:27 +0200

    setup of the Main server

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require('express'); 
 const app = express(); 
+const PORT = process.env.PORT || 3000;
 
-// Definition of the GET route for the URL
+// Definition of the GET route for the root URL
 app.get('/', (req, res) => {
 	res.send('PharmaNex Server is running');
 });
 
-app.listen(3000, '0.0.0.0', () => {
-  console.log('PharmaNex Server started on port 3000');
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`PharmaNex Server started on port ${PORT}`);
 });
