00a520018605070cb32792576eb8b32679f15168
Pritchad25 <pricts29@gmail.com>
2025-05-08 08:25:07 +0200

    connecting to the mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-const express = require('express'); 
+const express = require('express');
+const mongoose = require('mongoose'); 
 const app = express(); 
 const PORT = process.env.PORT || 3000;
 
@@ -7,6 +8,12 @@ app.get('/', (req, res) => {
 	res.send('PharmaNex Server is running');
 });
 
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+//Starting the Web Server
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`PharmaNex Server started on port ${PORT}`);
 });
