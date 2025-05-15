9a22e91e98f4bbabf47715ac130ffbceeada02df
Pritchad25 <pricts29@gmail.com>
2025-05-15 09:10:12 +0200

    feat: wiring the pharmacy routes & mounting centralized Express error handler

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,20 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./config/swagger');
 const patientRoutes = require('./routes/patients');
-
+const pharmacyRoutes = require('./routes/pharmacies');
+const errorHandler = require('./middleware/errorHandler');
 
 app.use(express.json());
 
 //Updating this file with routes from patients.js
 app.use('/api/patients', patientRoutes);
 
+//Updating this file with routes from pharmacies.js
+app.use('/api/pharmacies', pharmacyRoutes);
+
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+//Error handling middleware
+app.use(errorHandler);
 
 module.exports = app;
